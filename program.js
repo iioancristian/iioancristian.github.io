5fb10c534234fc8cecf5ffff41b866a810380448
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-10-30 18:35:51 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,14 +1,14 @@
-var electronic = false;
+var electric = false;
 function modifica()
 {
 document.getElementById("id_salut").innerHTML = "hello";
-  if(electronic == false) {
-    document.getElementById("img_id").src = "electric.jpg";
+  if(electric == false) {
+    document.getElementById("img_id").src = "electronic.jpg";
   
   }
   else
   {
-    document.getElementById("img_id").src = "electronic.jpg";
+    document.getElementById("img_id").src = "electric.jpg";
   }
 
 document.getElementById("img_id").src = "electric.jpg";
