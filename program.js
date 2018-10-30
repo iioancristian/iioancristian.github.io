0dde2fc4564a74a148ad095392709d95108a4106
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-10-30 18:33:37 +0200

    schimb poza

--- program.js
+++ program.js
@@ -1,6 +1,15 @@
+var electronic = false;
 function modifica()
 {
 document.getElementById("id_salut").innerHTML = "hello";
+  if(electronic == false) {
+    document.getElementById("img_id").src = "electric.jpg";
+  
+  }
+  else
+  {
+    document.getElementById("img_id").src = "electronic.jpg";
+  }
 
 document.getElementById("img_id").src = "electric.jpg";
 }
