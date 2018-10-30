b1c2135fecb089a10502ca9d8e7727c0d672a3d3
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-10-30 19:43:05 +0200

    desen

--- orientation.js
+++ orientation.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.0"
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.10.30.1"
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
 function la_schimbare_gyro(e)
@@ -6,5 +6,11 @@ function la_schimbare_gyro(e)
 document.getElementById("id_x").innerHTML = e.beta;
 document.getElementById("id_y").innerHTML = e.gamma;
 document.getElementById("id_z").innerHTML = e.alpha;
-
+  
+  var canvas = ducument.getElementById("canvas");
+  var context = canvas.getContext("2d");
+  
+  context.beginPath();
+  context.arc(canvas.width / 2, canvas.height / 2, 30, 0, 2* Math.PI);
+  context.stroke();
 }
