a72ae31221598dcec07b6f804e262fb115b7a5ca
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-11-07 01:22:36 +0200

    Update program.js

--- program.js
+++ program.js
@@ -1,15 +1,16 @@
-var electric = false;
+var electronic = false;
 function modifica()
 {
 document.getElementById("id_salut").innerHTML = "hello";
 document.getElementById("img_id").src = "electric.jpg";  
-  if(electric == false) {
-    document.getElementById("img_id").src = "electric.jpg";
+  
+  if(electronic == false) {
+    document.getElementById("img_id").src = "electronic.jpg";
   
   }
   else
   {
-    document.getElementById("img_id").src = "electronic.jpg";
+    document.getElementById("img_id").src = "electric.jpg";
   }
  
 }
