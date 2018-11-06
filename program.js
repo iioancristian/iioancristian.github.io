0e50e779d048e83c0a1459d23b3f6b19363b9c4e
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-11-06 23:54:19 +0200

    modif

--- program.js
+++ program.js
@@ -1,15 +1,15 @@
-var electronic.jpg = false;
+var electronic = false;
 function modifica()
 {
 document.getElementById("id_salut").innerHTML = "hello";
   
-  if(electronic.jpg == false) {
-    document.getElementById("img_id").src = "electric.jpg";
+  if(electronic == false) {
+    document.getElementById("img_id").src = "electronic.jpg";
   
   }
   else
   {
-    document.getElementById("img_id").src = "electronic.jpg";
+    document.getElementById("img_id").src = "electric.jpg";
   }
 
 }
