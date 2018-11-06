3aab16fdaf121fdfcb3c4407fa75285a4973f9cc
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-11-07 00:59:15 +0200

    Update program.js

--- program.js
+++ program.js
@@ -5,7 +5,7 @@ document.getElementById("id_salut").innerHTML = "hello";
 document.getElementById("img_id").src = "electric.jpg";
   
   if(electronic == false) {
-    document.getElementById("img_id").src = "electric.jpg";
+    document.getElementById("img_id").src = "electronic.jpg";
   
   }
   else
