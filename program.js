45e1e55236881f5c5f8c318603a75387dc09c303
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-11-07 01:18:05 +0200

    Update program.js

--- program.js
+++ program.js
@@ -4,12 +4,12 @@ function modifica()
 document.getElementById("id_salut").innerHTML = "hello";
 document.getElementById("img_id").src = "electric.jpg";  
   if(electric == false) {
-    document.getElementById("img_id").src = "electronic.jpg";
+    document.getElementById("img_id").src = "electric.jpg";
   
   }
   else
   {
-    document.getElementById("img_id").src = "electric.jpg";
+    document.getElementById("img_id").src = "electronic.jpg";
   }
  
 }
