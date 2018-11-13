14c230142c4190081df174806952f01bc133940c
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-11-13 18:42:01 +0200

    buton

--- program.js
+++ program.js
@@ -2,15 +2,18 @@ var electronic = false;
 function modifica()
 {
 document.getElementById("id_salut").innerHTML = "hello";
-document.getElementById("img_id").src = "electric.jpg";  
+
   
   if(electronic == false) {
-    document.getElementById("img_id").src = "electronic.jpg";
+    document.getElementById("img_id").src = "electric.jpg";
+    electronic = true;
   
   }
-  else
-  {
-    document.getElementById("img_id").src = "electric.jpg";
+  
+  
+  else{
+    document.getElementById("img_id").src = "electronic.jpg";
+    electric = false;
   }
  
 }
