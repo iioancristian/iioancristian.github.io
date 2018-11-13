ccf68ff782d6305b0c1fe3fd303bffa48a06b648
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-11-13 19:38:24 +0200

    print

--- cam.js
+++ cam.js
@@ -1,34 +1,37 @@
-document.getElementById("id_logic_version").innerHTML = "logic version: 2018.11.13.3";
-document.getElementById("id_video").addEventListener("mousedown",on_touch);
-document.getElementById("id_video").addEventListener("touchstart",on_touch);
-//-------------------------------------------------------------------
+document.getElementById("id_logic_version").innerHTML= "Logic version: 2018.11.13.0";
+document.getElementById("id_video").addEventListener("mousedown",on_touch );
+document.getElementById("id_video").addEventListener("touchstart",on_touch );
+
+//-------------------
 function on_ok(stream)
 {
 	document.getElementById("id_video").srcObject = stream;
 	
 	
 }
-//--------------------------------------------------------------------
+//-------------------
 function on_error(e)
 {
-    //console.log("eroare camera");	
-	alert("eroare camera");
-	
-	
+	//console.log("eroare camera");
+		alert("eraore camera");
+		
 }
-//---------------------------------------------------------------------
+//-------------------
+
 function start()
 {
-	var p = {audio: true, video: true};
+	var p = {audio:true, video:true};
+		
 	navigator.mediaDevices.getUserMedia(p).then(on_ok).catch(on_error);
 	
 }
-//--------------------------------------------------------------------
+//------------------
 function on_touch(e)
-	{
-		var canvas = document.getElementById("id_canvas");
-		var context = canvas.getContext("2d");
-		
-		var video = document.getElementById("id_video");
-		context.drawImage(video,0,0)
-	}
+{
+	var canvas = document.getElementById("id_canvas");
+	var context = canvas.getContext("2d");
+	var video= document.getElementById("id_video");
+	context.drawImage(video,0,0);
+	
+	
+}
