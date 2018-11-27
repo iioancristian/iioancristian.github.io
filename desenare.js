aea6247e861d454e8c93414d452541e0a79dc821
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-11-27 17:44:43 +0200

    micsorare grosime linie

--- desenare.js
+++ desenare.js
@@ -1,5 +1,5 @@
 document.getElementById("id_logic_version").innerHTML = 
-		"Logic version = 2018.11.20.5";
+		"Logic version = 2018.11.27.0";
 		
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
@@ -20,7 +20,7 @@ function on_touch(e)
 		context.beginPath();
 		context.arc(e.changedTouches.item(i).pageX - rect.left,
 					e.changedTouches.item(i).pageY - rect.top,
-					20,
+					10,
 					0, 2 * Math.PI
 					);
 		context.stroke();
@@ -39,10 +39,13 @@ function on_touch_move(e)
 		context.lineWidth = 1;
 		context.arc(e.changedTouches.item(i).pageX - rect.left,
 					e.changedTouches.item(i).pageY - rect.top,
-					20,
+					10,
 					0, 2 * Math.PI
 					);
-		context.lineWidth = 40;			
+		context.stroke();
+		
+		context.beginPath();
+		context.lineWidth = 20;			
 		context.moveTo(lastX - rect.left, lastY - rect.top);
 		context.lineTo(e.changedTouches.item(i).pageX - rect.left, 
 						e.changedTouches.item(i).pageY - rect.top);
