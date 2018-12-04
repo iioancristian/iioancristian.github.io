9827f10ffdf9f4a859c77ceb1743809935b4ef5c
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-12-04 18:06:15 +0200

    3

--- synth.js
+++ synth.js
@@ -1,6 +1,8 @@
-document.getElementById("id_logic_version").innerHTML="Logic version: 1";
+document.getElementById("id_logic_version").innerHTML="Logic version: 2018.12.04.3";
 var synth = window.speechSynthesies;
 
+
+//--------------------------------
 function afisaza_voci()
 {
 	
@@ -8,6 +10,16 @@ function afisaza_voci()
 	
 	for (var i = 0; i<lista_voci.length;i++)
 		document.getElementById("id_voci").innerHTML += lista_voci[i].lang + "  " + lista_voci[i].name +"<br>";
-	
+		
+}
+//--------------------------------------
+function vorbeste()
+{
+	var enunt = new speechSynthesiesUtterance();
+	enunt.text = document.getElementById("id_text").value;
+	enunt.language = "en-US";
+	synth.speak(enunt);
 	
 }
+
+//-------------------------------------
