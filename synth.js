3d142d911c29c6b1906424749fa49b26921c5b9a
iioancristian <44406510+iioancristian@users.noreply.github.com>
2018-12-04 18:45:42 +0200

    8

--- synth.js
+++ synth.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.7";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.04.8";
 var synth = window.speechSynthesis;
 
 //----------------------------------
@@ -16,7 +16,14 @@ function vorbeste()
 	var enunt = new SpeechSynthesisUtterance();
 	enunt.text = document.getElementById("id_text").value;
 	enunt.lang = "en-US";
+	enunt.onend = sfarsit_vorbeste;
+	document.getElementById("id_button_vorbeste").disable = true;
 	synth.speak(enunt);
-	 utterThis.rate = 2.5;
+	
 }
 //----------------------------------
+function sfarsit_vorbeste()
+{
+	document.getElementById("id_button_vorbeste").disable = false;
+	
+}
