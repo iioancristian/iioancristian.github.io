d961bdfc0a63c1985c212fce3cf937aa78db56a0
iioancristian <44406510+iioancristian@users.noreply.github.com>
2019-01-08 19:08:59 +0200

    4

--- touchmove.js
+++ touchmove.js
@@ -1,15 +1,23 @@
 document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.1";
 var svg = document.getElementById("id_svg");
-var cerc = document.getElementById("id_circle");
+var cerc1 = document.getElementById("id_circle1");
+var cerc2 = document.getElementById("id_circle2");
 
-cerc.addEventListener("touchmove",on_touch_move);
+cerc1.addEventListener("touchmove", on_touch_move1);
+cerc2.addEventListener("touchmove", on_touch_move2);
+
 var svg_rect = svg.getBoundingClientRect();
 
-function on_touch_move(e)
+function on_touch_move1(e)
 {
 	e.preventDefault();
+cerc1.setAttribute("cx", e.changedTouches[0].pageX);
+cerc1.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
+}
 
-	cerc.setAttribute("cx", e.changedTouches[0].pageX);
-	cerc.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
-		
+function on_touch_move2(e)
+{
+	e.preventDefault();
+cerc2.setAttribute("cx", e.changedTouches[0].pageX);
+cerc2.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
 }
